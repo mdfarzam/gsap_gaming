f486d535caf46db7931f7c8640083a78a3c79ddd
mdfarzam <mohammedfarzam02@gmail.com>
2025-11-12 00:41:45 +0530

    animated the title

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
 
 export default function App() {
@@ -5,8 +6,7 @@ export default function App() {
     <main className="relative min-h-screen w-screen overflow-x-hidden">
 
       <Hero/>
-
-      <section className="z-0 min-h-screen bg-blue-500 " />
+      <About/>
 
     </main>
   );
